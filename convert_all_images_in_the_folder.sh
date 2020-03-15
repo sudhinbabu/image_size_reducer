821fcac9041d4914bae4add6184d920552f225c6
sudhin babu <zudhinn@gmail.com>
2020-03-15 21:37:24 +0530

    Update convert_all_images_in_the_folder.sh

--- convert_all_images_in_the_folder.sh
+++ convert_all_images_in_the_folder.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-# install imagemagick
- 
-mkdir -p converted
-for f in *.JPG ; do 
-    echo $f;
-    convert $f -quality 75 converted/$f ; 
 
+quality=${2:-75};
+if [[ -d "$1" ]] 
+then
+    prefix=${1%/}
+else
+    echo "Invalid or no perfix passed setting prefix=$(pwd)";
+    prefix=$(pwd)
+    
+fi
+
+
+mkdir -p $prefix"/converted";
+for f in $prefix/*.JPG ; do 
+    # convert $f -quality 75 $prefix/converted/$f ; 
+    filename=${f##*/}
+    echo "convert $f -quality $quality $prefix/converted/$filename" ; 
 done
